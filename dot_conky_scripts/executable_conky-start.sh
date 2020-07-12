ab7ca6b073e7c94c5904b7b925464e73232d8b2d
vogu66 <24728720+vogu66@users.noreply.github.com>
2020-07-12 15:56:07 +0200

    add moving icons to the theme

--- dot_conky_scripts/executable_conky-start.sh
+++ dot_conky_scripts/executable_conky-start.sh
@@ -1,21 +1,43 @@
 #!/bin/zsh
 
-# eventually, should detect whether conky runs already before deciding whther to sleep
-sleep 61s
+# TODO eventually, should detect whether conky runs already before deciding
+# whether to sleep
+#sleep 56s
 killall conky
 
 # select theme
-# for now -- eventually, should select randomly
+# TODO for now -- eventually, should select randomly
 theme="redhead"
-# eventually, should differentiate between single and double screen, too
+# TODO eventually, should differentiate between single and double screen, too
+# or screen rotated or resolution or whatever
+
+if [ -f ~/.conky_scripts/last_theme ]
+then
+	# read old theme in file
+	old=$(cat ~/.conky_scripts/last_theme)
+	if [ ! -e ~/.conky_scripts/$old/icon_config ]
+	then
+		mkdir ~/.conky_scripts/$old/icon_config
+	fi
+	# save current icon positions to the right theme folder
+	cp -rf /home/vogu/.config/xfce4/desktop/* ~/.conky_scripts/$old/icon_config
+fi
+
+# save current theme in file
+echo $theme >! ~/.conky_scripts/last_theme
 
 # → in the theme folder
+
+# set icon positions to what's in the file
+cp -rf ~/.conky_scripts/$theme/icon_config/* /home/vogu/.config/xfce4/desktop
+# press f5 to update because that thing doesn't work and xfdesktop --reload doesn't either
+
 # in another script
+~/.conky_scripts/$theme/setup.sh
 # works for xfce
 # 	set wallpaper
 # 	set options for wallpaper
 #   just copy the configuration from settings editor
-~/.conky_scripts/$theme/setup.sh
 
 # sleep 5s # to be sure the computer has time to update everything
 		 # although it shouldn't be necessary
